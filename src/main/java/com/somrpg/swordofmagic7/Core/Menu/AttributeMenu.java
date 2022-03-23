4c5a620eb0deb8551ecdbef5862038f1fdebeb69
MomiNeko <rikuton2002@gmail.com>
2022-03-23 17:16:00 +0900

    AttributeMenuのバグ集

--- src/main/java/com/somrpg/swordofmagic7/Core/Menu/AttributeMenu.java
+++ src/main/java/com/somrpg/swordofmagic7/Core/Menu/AttributeMenu.java
@@ -40,27 +40,38 @@ public interface AttributeMenu extends BaseMenu {
         loreSTR.add(decoAttributeLore("物理ダメージ倍率", GenericConfig.AttributeValue_STR_DamageATK));
         loreSTR.add(decoAttributeLore("攻撃力", GenericConfig.AttributeValue_STR_ATK));
         content[0] = ViewableItemStack.create("筋力", Material.RED_DYE, loreSTR).viewItemStack();
-        loreSTR.add(decoAttributeLore("魔法ダメージ倍率", GenericConfig.AttributeValue_INT_DamageMAT));
-        loreSTR.add(decoAttributeLore("魔法ダメージ軽減", GenericConfig.AttributeValue_INT_ResistMAT));
-        loreSTR.add(decoAttributeLore("攻撃力", GenericConfig.AttributeValue_INT_ATK));
-        content[1] = ViewableItemStack.create("魔力", Material.PURPLE_DYE, loreSTR).viewItemStack();
-        loreSTR.add(decoAttributeLore("回避", GenericConfig.AttributeValue_DEX_EVA));
-        loreSTR.add(decoAttributeLore("クリティカルダメージ", GenericConfig.AttributeValue_DEX_CriticalDamage));
-        content[2] = ViewableItemStack.create("敏捷", Material.YELLOW_DYE, loreSTR).viewItemStack();
-        loreSTR.add(decoAttributeLore("命中", GenericConfig.AttributeValue_TEC_ACC));
-        loreSTR.add(decoAttributeLore("クリティカル発生", GenericConfig.AttributeValue_TEC_CriticalRate));
-        content[3] = ViewableItemStack.create("技量", Material.LIME_DYE, loreSTR).viewItemStack();
-        loreSTR.add(decoAttributeLore("最大マナ", GenericConfig.AttributeValue_SPI_MaxMana));
-        loreSTR.add(decoAttributeLore("マナ自動回復", GenericConfig.AttributeValue_SPI_ManaRegen));
-        loreSTR.add(decoAttributeLore("治癒力", GenericConfig.AttributeValue_SPI_HLP));
-        loreSTR.add(decoAttributeLore("魔法ダメージ軽減", GenericConfig.AttributeValue_SPI_ResistMAT));
-        content[4] = ViewableItemStack.create("精神", Material.LIGHT_BLUE_DYE, loreSTR).viewItemStack();
-        loreSTR.add(decoAttributeLore("最大体力", GenericConfig.AttributeValue_VIT_MaxHealth));
-        loreSTR.add(decoAttributeLore("体力自動回復", GenericConfig.AttributeValue_VIT_HealthRegen));
-        loreSTR.add(decoAttributeLore("防御力", GenericConfig.AttributeValue_VIT_DEF));
-        loreSTR.add(decoAttributeLore("物理ダメージ軽減", GenericConfig.AttributeValue_VIT_ResistATK));
-        loreSTR.add(decoAttributeLore("クリティカル耐性", GenericConfig.AttributeValue_VIT_CriticalResist));
-        content[5] = ViewableItemStack.create("活力", Material.ORANGE_DYE, loreSTR).viewItemStack();
+        
+        List<String> loreINT = new ArrayList<>();
+        loreINT.add(decoAttributeLore("魔法ダメージ倍率", GenericConfig.AttributeValue_INT_DamageMAT));
+        loreINT.add(decoAttributeLore("魔法ダメージ軽減", GenericConfig.AttributeValue_INT_ResistMAT));
+        loreINT.add(decoAttributeLore("攻撃力", GenericConfig.AttributeValue_INT_ATK));
+        content[1] = ViewableItemStack.create("魔力", Material.PURPLE_DYE, loreINT).viewItemStack();
+        
+        List<String> loreDEX = new ArrayList<>();
+        loreDEX.add(decoAttributeLore("回避", GenericConfig.AttributeValue_DEX_EVA));
+        loreDEX.add(decoAttributeLore("クリティカルダメージ", GenericConfig.AttributeValue_DEX_CriticalDamage));
+        content[2] = ViewableItemStack.create("敏捷", Material.YELLOW_DYE, loreDEX).viewItemStack();
+
+        List<String> loreTEC = new ArrayList<>();
+        loreTEC.add(decoAttributeLore("命中", GenericConfig.AttributeValue_TEC_ACC));
+        loreTEC.add(decoAttributeLore("クリティカル発生", GenericConfig.AttributeValue_TEC_CriticalRate));
+        content[3] = ViewableItemStack.create("技量", Material.LIME_DYE, loreTEC).viewItemStack();
+
+        List<String> loreSPI = new ArrayList<>();
+        loreSPI.add(decoAttributeLore("最大マナ", GenericConfig.AttributeValue_SPI_MaxMana));
+        loreSPI.add(decoAttributeLore("マナ自動回復", GenericConfig.AttributeValue_SPI_ManaRegen));
+        loreSPI.add(decoAttributeLore("治癒力", GenericConfig.AttributeValue_SPI_HLP));
+        loreSPI.add(decoAttributeLore("魔法ダメージ軽減", GenericConfig.AttributeValue_SPI_ResistMAT));
+        content[4] = ViewableItemStack.create("精神", Material.LIGHT_BLUE_DYE, loreSPI).viewItemStack();
+
+        List<String> loreVIT = new ArrayList<>();
+        loreVIT.add(decoAttributeLore("最大体力", GenericConfig.AttributeValue_VIT_MaxHealth));
+        loreVIT.add(decoAttributeLore("体力自動回復", GenericConfig.AttributeValue_VIT_HealthRegen));
+        loreVIT.add(decoAttributeLore("防御力", GenericConfig.AttributeValue_VIT_DEF));
+        loreVIT.add(decoAttributeLore("物理ダメージ軽減", GenericConfig.AttributeValue_VIT_ResistATK));
+        loreVIT.add(decoAttributeLore("クリティカル耐性", GenericConfig.AttributeValue_VIT_CriticalResist));
+        content[5] = ViewableItemStack.create("活力", Material.ORANGE_DYE, loreVIT).viewItemStack();
+        
         return content;
     }
 
